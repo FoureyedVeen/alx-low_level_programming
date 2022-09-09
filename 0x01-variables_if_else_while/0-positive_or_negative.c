2004e309f1f0fc74100fe04a61907386ac50c317
“FoureyedVeen” <cosmasdaniella@gmail.com>
2022-09-09 11:25:17 -0700

    positive

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	int n;
+
+	if (n > 0)
+	{
+		printf("Last digit of %d is %d and is greater than 5,\n" n, lastDigit);
+	}
+	else if (n == 0)
+	{
+		printf("Last digit of %d is %d and is zero,\n" n,lastDigit);
+	}
+	else (n < 0)
+	{
+		printf("Last digit of %d is %d and is less than 6 not 0,\n" n, lastDigit);
+	}
+}
