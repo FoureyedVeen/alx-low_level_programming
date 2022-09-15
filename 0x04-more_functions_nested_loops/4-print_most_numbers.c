047c6c8186658f863c4ce9e635290cf86a8a831b
“FoureyedVeen” <cosmasdaniella@gmail.com>
2022-09-15 16:09:31 -0700

    fix

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,14 @@
  */
 void print_numbers(void)
 {
-	int i;
+	char i;
 
 	for (i = '0'; i <= '9'; i++)
 	{
-		if (i != '2' && i != '4')
+		if (!(i == '2' || i == '4'))
 		{
 			_putchar(i);
 		}
 	}
-	_putchar("\n");
+	_putchar('\n');
 }
