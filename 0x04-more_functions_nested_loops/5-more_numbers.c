cca7e0fae89ca5c688d6ef2d44be81fdbcda87ac
“FoureyedVeen” <cosmasdaniella@gmail.com>
2022-09-15 16:53:15 -0700

    one more

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,15 +1,16 @@
 #include "main.h"
 
 /**
- * more_numbers - numbers
+ * more_numbers - nmbr fct
  *
  * Return: no return
  */
+
 void more_numbers(void)
 {
-	int i, j;
 
-	for (i = 0; i <= 10; i++)
+int i, j;
+	for (i = 1; i <= 10; i++)
 	{
 		for (j = 0; j <= 14; j++)
 		{
@@ -17,6 +18,6 @@ void more_numbers(void)
 			_putchar('1');
 			_putchar(j % 10 + '0');
 		}
-		_putchar('\n');
-	}
+			_putchar('\n');
+		}
 }
