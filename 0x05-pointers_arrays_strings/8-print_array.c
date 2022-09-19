36f7e76021e146e92bc90da3c16c2a5c759c80aa
“FoureyedVeen” <cosmasdaniella@gmail.com>
2022-09-19 16:25:47 -0700

    array again

--- 0x05-pointers_arrays_strings/8-print_array.c
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -10,11 +10,14 @@
  */
 void print_array(int *a, int n)
 {
-	int i;
-
-	for (i = 0; i < (n - 1); i++)
-		printf("%d, ", a[i]);
-		if (i == (n - 1))
-			printf("%d", a[n - 1]);
-	printf("\n");
+	if (a)
+	{
+		while (n > 0)
+		{
+			printf("%d", *a++);
+			if (--n)
+				printf(", ");
+		}
+		_putchar('\n');
+	}
 }
