13fe923c77f92241d15d2745a994ae9a24b2639d
“FoureyedVeen” <cosmasdaniella@gmail.com>
2022-09-19 17:55:59 -0700

    dannie

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,12 +8,19 @@
  */
 void put2(char *str)
 {
-	if (str)
+	int i = 0;
+
+	while (i >= 0)
 	{
-		do {
-			if (*str)
-				_putchar(*str++);
-		} while (*str++);
-			_putchar('\n');
+		if (str[i] == '\0')
+			{
+				_putchar('\n');
+				break;
+			}
+			if (i % 2 == 0)
+			_putchar(str[i]);
+			i++;
+				
+			
 	}
 }
