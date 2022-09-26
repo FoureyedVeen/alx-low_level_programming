542d54774525007618de001e0ec4499c959e6031
“FoureyedVeen” <cosmasdaniella@gmail.com>
2022-09-26 14:20:10 -0700

    array

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strchr - a function that locates a character in a string
+ * @s: an input string to search in
+ * @c: an input character to locate into string s
+ * Return: returns pointer to character c
+ *
+ */
+char *_strchr(char *s, char c)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] == c)
+		{
+			return (s + i);
+		}
+
+	}
+	if (s[i] == c)
+		return (s + i);
+	return (0);
+}
